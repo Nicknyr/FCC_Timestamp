5580f1f297ea7f4f1a0edb8094a09dfdeb642b2b
Nick Kinlen <nick_kinlen@hotmail.com>
2018-05-09 17:37:20 -0400

    Basic Node/Express server set up

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var app = module.exports = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.listen(3000, function(){
+  console.log('App running on port 3000');
+});
